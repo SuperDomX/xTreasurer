7ca30e21d48176afe3dee81334f8bf739e420294
xopherdeep <heylisten@xtiv.net>
2014-08-26 00:43:41 -0700

    xTreasurer/ Automated Nightly Push

--- xTreasurer.php
+++ xTreasurer.php
@@ -59,7 +59,7 @@
 			// Set your secret key: remember to change this to your live secret key in production
 			// See your keys here https://dashboard.stripe.com/account
 
-			$CFG = $this->readConfigs();
+			$CFG = $this->readConfigs($this->_SET['action']);
  
 			$key = "stripe_$CFG[stripe_live_test]_secret";
 			
